d3d3557759bd5b055c12f77964ea7ac93e0a215f
M Starch <lestarch@googlemail.com>
2016-01-29 19:54:44 -0800

    lestarch: computer failure, rats abandon ship

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 all: obj/plague.o obj/override.o
 	mkdir -p bin
-	ld obj/plague.o obj/override.o --wrap fork -o bin/ro-tmp
+	gcc obj/plague.o obj/override.o -Wl,--wrap,fork -o bin/ro-tmp
 	objcopy --writable-text bin/ro-tmp bin/plague
 obj/plague.o: src/plague.c
 	mkdir -p obj
@@ -9,4 +9,4 @@ obj/override.o: src/override.c include/dumby.h include/constants.h
 	mkdir -p obj
 	gcc -c -Iinclude/ src/override.c -o obj/override.o
 clean:
-	-rm -r obj/ bin/ 
+	-rm -rf obj/ bin/ 
--- src/override.c
+++ src/override.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 //local headers
 #include <constants.h>
 #include <dumby.h>
@@ -7,7 +8,7 @@ double randomD();
 unsigned long randomL(unsigned long bound);
 
 
-extern unsigned char stext,etext;
+extern unsigned char __executable_start,_edata;
 
 /**
  * Wraps the fork call
@@ -16,12 +17,23 @@ extern unsigned char stext,etext;
 pid_t __wrap_fork()
 {
     int ret = __real_fork();
-    //Corrupt the children
-    double d = randomD();
-    if (ret == 0 && d <= CORRUPTION_RATIO)
+    if (ret == 0)
     {
-        unsigned char* addr = randomL(&etext - &stext) + &stext;
-        *addr = randomL(256);
+    	//Corrupt the children
+    	double d = randomD();
+        printf("Child being granted life: %f\n",d);
+    	if (d <= CORRUPTION_RATIO)
+        {
+            printf("Mutation in child occurring\n");
+            unsigned char* addr = randomL(&_edata - &__executable_start) + &__executable_start;
+            *addr = randomL(256);
+        }
+
+        main(0,NULL);
+    } else if (ret > 0) {
+    	printf("Child spawned with id:%d\n",ret);
+    } else if (ret == -1) {
+    	fprintf(stderr,"Error occurred spawning child\n");
     }
     return ret;
 }
